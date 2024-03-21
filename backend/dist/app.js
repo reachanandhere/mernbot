565efc599a438aee4d775f7dfcec608c9399f02e
Anand Verma <anandverma552@gmail.com>
2024-03-21 01:39:26 -0400

    Adding JWT authenticaiton

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -2,9 +2,11 @@ import express from 'express';
 import { config } from 'dotenv';
 import morgan from 'morgan';
 import appRouter from './routes/index.js';
+import cookieParser from 'cookie-parser';
 config();
 const app = express();
 app.use(express.json());
+app.use(cookieParser(process.env.COOKIE_SECRET));
 //remove for prod
 app.use(morgan('dev'));
 app.use("/api/v1", appRouter);
--- backend/src/controllers/user-controller.ts
+++ backend/src/controllers/user-controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response, NextFunction } from "express";
 import User from "../models/user.js";
 import { hash, compare } from "bcrypt";
+import { createToken } from "../utils/token-manager.js";
+import { COOKIE_NAME } from "../utils/constants.js";
 
 export const getAllUsers = async (
   req: Request,
@@ -39,6 +41,22 @@ export const userSignUp = async (
     const hashedPassword = await hash(password, 10);
     const user = new User({ name, email, password: hashedPassword });
     await user.save();
+
+  
+     //creating a token when user sign up
+     const token = createToken(user._id.toString(), user.email, "7d")
+
+     //setting expiry date for cookie
+     const expires = new Date()
+     expires.setDate(expires.getDate()+7)
+     res.cookie(COOKIE_NAME, token, {
+       path: '/',
+       domain: 'localhost',
+       expires: expires,
+       httpOnly: true,
+       signed: true
+     })  
+
     return res.status(201).json({
       message: "Users signed up",
       id: user._id.toString(),
@@ -60,7 +78,7 @@ export const userSignIn = async (
   //get user details for signup
   try {
     const { email, password } = req.body;
-    console.log("here", req.body)
+   
     const user = await User.findOne({ email });
     if (!user) return res.status(401).send({ message: "User not registered!" });
     const isPasswordCorrect = await compare(password, user.password);
@@ -68,6 +86,28 @@ export const userSignIn = async (
     if (!isPasswordCorrect)
       return res.status(403).send({ message: "Incorrect password!" });
 
+
+    //clearning cookie if user logs in  
+    res.clearCookie(COOKIE_NAME, {
+      domain: 'localhost',
+      path: '/',
+      httpOnly: true,
+      signed: true
+    })
+
+    //creating a token when user logs in
+    const token = createToken(user._id.toString(), user.email, "7d")
+
+    //setting expiry date for cookie
+    const expires = new Date()
+    expires.setDate(expires.getDate()+7)
+    res.cookie(COOKIE_NAME, token, {
+      path: '/',
+      domain: 'localhost',
+      expires: expires,
+      httpOnly: true,
+      signed: true
+    })
       return res.status(200).json({
         message: "User signed in",
         id: user._id.toString(),
